38377943947d8ea27ad0741644e7dcac3c83dc98
Shadman Wasif <shadman.wasif@gmail.com>
2025-10-02 12:29:45 +0600

    Initial commit with the configuration

---
+++ mediumclone_api/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *  # noqa
+from .base import env
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="nWkwtH2RPfhCYkH_nfpfLixu81sU-dDNEKNQ1qwSRRt6VbP7s-s",
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
